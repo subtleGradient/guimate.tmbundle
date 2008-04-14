5bcf981c3b98ee58a3745b9dc37a7bbd2a4f4158
Thomas Aylott <oblivious+git@subtlegradient.com>
2008-04-13 20:25:24 -0400

    [NEW] GUI-esque style; removed document.write

--- Support/git.gui.js
+++ Support/git.gui.js
@@ -1,40 +1,47 @@
 try{if(TextMate && TextMate.system){
 
-
-baseelement = document.body;
-logElement = document.createElement('pre'); logElement.appendChild(document.createTextNode('')); logElement.setAttribute('id','log'); baseelement.appendChild(logElement);
-errElement = document.createElement('pre'); errElement.appendChild(document.createTextNode('')); errElement.setAttribute('id','err'); baseelement.appendChild(errElement);
+logElement = document.querySelector('#log .log');
+errElement = document.querySelector('#log .errors');
+toolbar = document.querySelector('#toolbar');
 
 // COMMANDS
 // TODO: Add a nice way to add a button for a command in the Class itself
-document.write('<h2>'+TextMate.system('echo "$TM_PROJECT_DIRECTORY"',null).outputString.replace('','')+'</h2>');
+document.querySelector('#context').innerHTML = ('<h2>'+TextMate.system('echo "$TM_PROJECT_DIRECTORY"',null).outputString.replace('','')+'</h2>');
 
 var edit   = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" edit',     { logElement: errElement, errElement: errElement });
-document.write('<input type="button" value="edit" onclick="edit.run().close()" style="position:absolute;top:10px;right:10px;z-index:9999" />');
+toolbar.innerHTML += ('<input type="button" value="edit" onclick="edit.run().close()" />');
 
 var hash = { logElement: logElement, errElement: errElement };
 
 // Git commands
-document.write(' Info:');
-var diff   = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" diff', { logElement: errElement, errElement: errElement });document.write('<input accesskey="d" type="button" value="[D]iff" onclick="diff.run().close()" />');
-var st     = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" status', hash                                                                   );document.write('<input accesskey="i" type="button" value="Status [i]" onclick="st.clear();st.run()" style="font-size:2em;" />');
-var GitNub = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" nub', { logElement: errElement, errElement: errElement }        );document.write('<input accesskey="n" type="button" value="Git[N]ub" onclick="GitNub.run().close()" />');
-document.write('<input accesskey="k" type="button" value="[K]ill Log"  onclick="st.clear();" style="font-size:0.5em" />');
+toolbar.innerHTML += (' Info:');
+var diff = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" diff', { logElement: errElement, errElement: errElement });
+toolbar.innerHTML += ('<input accesskey="d" type="button" value="[D]iff" onclick="diff.run().close()" />');
+var st = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" status', hash );
+toolbar.innerHTML += ('<input accesskey="i" type="button" value="Status [i]" onclick="st.clear();st.run()" />');
+var GitNub = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" nub', { logElement: errElement, errElement: errElement } );
+toolbar.innerHTML += ('<input accesskey="n" type="button" value="Git[N]ub" onclick="GitNub.run().close()" />');
+toolbar.innerHTML += ('<input accesskey="k" type="button" value="[K]ill Log" onclick="st.clear();" />');
 
-document.write(' Commit:');
-var addall = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" addall', hash );document.write('<input accesskey="a" type="button" value="[A]dd * " onclick="addall.clear();addall.run()" />');
-var ci     = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" commit', hash );document.write('<input accesskey="s" type="button" value="[s] Commit…" onclick="ci.clear();ci.run()" />');
+toolbar.innerHTML += (' Commit:');
+var addall = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" addall!', hash );
+toolbar.innerHTML += ('<input accesskey="a" type="button" value="[A]dd * " onclick="addall.clear();addall.run()" />');
+var ci = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" commit', hash );
+toolbar.innerHTML += ('<input accesskey="s" type="button" value="[s] Commit…" onclick="ci.clear();ci.run()" />');
 
-document.write(' Push:');
-var push   = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" push!', hash );document.write('<input accesskey="u" type="button" value="P[u]sh to origin" onclick="push.clear();push.run()" />');
-var stage  = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" stage!',  hash );document.write('<input type="button" value="Push to \'stage\'" onclick="stage.clear();stage.run()" />');
+toolbar.innerHTML += (' Push:');
+var push = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" push!', hash );
+toolbar.innerHTML += ('<input accesskey="u" type="button" value="P[u]sh to origin" onclick="push.clear();push.run()" />');
+var stage = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" stage!', hash );
+toolbar.innerHTML += ('<input type="button" value="Push to \'stage\'" onclick="stage.clear();stage.run()" />');
 
 // diff.run();
-new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" default', hash ).run();
+st.run();
+// new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" default', hash ).run();
 
 /*
 ${10:diff}   = new ShellScript('"$TM_BUNDLE_SUPPORT/git.gui.rb" method_name',     hash);
-document.write('<input type="button" value="$10"  onclick="$10.clear();$10.run().close()" />')
+toolbar.innerHTML += ('<input type="button" value="$10"  onclick="$10.clear();$10.run().close()" />')
 */
 
 
